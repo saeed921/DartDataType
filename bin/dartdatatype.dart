7d54940270aa1f28f233a34915ecb38ba9007c9d
Saeed <saeedofficial921@gmail.com>
2022-06-11 16:55:23 +0600

    all kinds of data type in dart

---
+++ bin/dartdatatype.dart
@@ -0,0 +1,8 @@
+import 'package:dartdatatype/dartdatatype.dart' as dartdatatype;
+
+void main() {
+
+  print('Hello world:');
+
+
+}
---
+++ test/dartdatatype_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartdatatype/dartdatatype.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
